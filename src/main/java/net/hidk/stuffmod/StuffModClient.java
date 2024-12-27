3e2c702f202cd6735b1cb3562132aad30efd0fb2
Anthony Smith <powerofthewolfie@gmail.com>
2024-12-27 11:24:33 -0500

    Initial commit

---
+++ src/main/java/net/hidk/stuffmod/StuffModClient.java
@@ -0,0 +1,10 @@
+package net.hidk.stuffmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StuffModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
